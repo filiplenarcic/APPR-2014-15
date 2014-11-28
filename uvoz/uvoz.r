e5f99c6dd943ef14d3354050be7faf85fb7244d4
Filip Lenarcic <filip.lenarcic@amis.net>
2014-11-28 20:20:53 +0100

    Uvoz podatkov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -17,3 +17,17 @@ druzine <- uvoziDruzine()
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+
+
+#tabela 1
+
+uvozi1 <- function(){
+  return(read.csv("podatki/tabela1.csv", header = FALSE sep = ";", as.is = TRUE, 
+                  row.names = 1), na.strings = "NA", skip = 3,
+         col.names = c("Dejavnost", "Regija", "Prihodki od prodaje (1000 EUR) 08", "Prihodki od prodaje (1000 EUR) 09", "Prihodki od prodaje (1000 EUR) 10", "Prihodki od prodaje (1000 EUR) 11", "Prihodki od prodaje (1000 EUR) 12", 
+                       "Plače (1000 EUR) 08", "Plače (1000 EUR) 09" "Plače (1000 EUR) 10", "Plače (1000 EUR) 11", "Plače (1000 EUR) 12", 
+                       "Zaposleni 08", "Zaposleni 09", "Zaposleni 10", "Zaposleni 11", "Zaposleni 12"), 
+         fileEncoding = "Windows-1250"
+}
+cat("Uvažam podatke o kohezijskih regijah podjetij po dejavnosti...\n")
+podjetjaRegija <- uvozi1()
